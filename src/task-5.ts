890acec1499a584509fba5ad6d67fbf7c85982c7
Sliliia85 <liliyasimbukhova@gmail.com>
2025-07-12 17:13:44 +0200

    my work

---
+++ src/task-5.ts
@@ -0,0 +1,17 @@
+enum Status {
+    Loading = "loading",
+    Success = "success",
+    Error = "error"
+}
+
+function logStatus(status: Status): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
